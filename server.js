ca2264c6b81d51f6c597436f2e9bfaec18a98164
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-12-19 18:23:48 -0800

    setup API server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-// const routes = require('./routes');
+const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //Add routes
-// app.use(routes);
+app.use(routes);
 
 //Connect to Mongo DB
 mongoose.connect(
